214da28f4d25a316d1699336b8d02e39123f0d01
vindiniii <vandanaman43@gmail.com>
2023-02-05 22:06:14 -0500

    initial commit

---
+++ assessment.js
@@ -0,0 +1,8 @@
+var firstName = "Vandana";
+var lastName = "Manchikanti";
+var age = 16;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+console.log("I am having so much fun in class!");
